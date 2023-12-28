b1ca4d8b127d5d8f39ad4fc1b3e8032df53064f2
keoni <kimkeonhwi991231@gmail.com>
2023-12-28 23:35:10 +0900

    asdf

--- src/main/java/hello/itemservice/TestDataInit.java
+++ src/main/java/hello/itemservice/TestDataInit.java
@@ -10,7 +10,6 @@ import javax.annotation.PostConstruct;
 @Component
 @RequiredArgsConstructor
 public class TestDataInit {
-
     private final ItemRepository itemRepository;
 
     /**
@@ -21,5 +20,4 @@ public class TestDataInit {
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
     }
-
 }
